e3c9121007cbeb6178eeab1ade21cc36ee91946c
priyank <priyank.tyagi2005@gmail.com>
2026-07-05 19:13:45 +0530

    chore: remove all project files and configurations

--- src/tools/list/underline-text-generator/logic.ts
+++
@@ -1,9 +0,0 @@
-import { UNDERLINE_MARKS, applyCombiningMark } from '../../utils/unicode';
-
-export type UnderlineStyle = 'single' | 'double' | 'wave' | 'dotted' | 'slash';
-
-export function underlineText(text: string, style: UnderlineStyle): string {
-  if (!text) return '';
-  const mark = UNDERLINE_MARKS[style] || UNDERLINE_MARKS.single;
-  return applyCombiningMark(text, mark);
-}
